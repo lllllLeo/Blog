fce9d7cff066757f28396dd4c1fd63776857c3f6
lllllLeo <andwithyj@gmail.com>
2019-07-16 23:03:11 +0900

    portfolio(spring) -> spring boot 로 바꾸기 전

--- leoBlog/src/main/java/blog/yujun/leo/controller/MainController.java
+++ leoBlog/src/main/java/blog/yujun/leo/controller/MainController.java
@@ -3,18 +3,20 @@ package blog.yujun.leo.controller;
 import blog.yujun.leo.dto.UserDTO;
 import blog.yujun.leo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class MainController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("yujun")
-    public UserDTO yujun() throws Exception {
+    public @ResponseBody
+    UserDTO yujun() throws Exception {
         return userService.getUserInfo();
     }
 
